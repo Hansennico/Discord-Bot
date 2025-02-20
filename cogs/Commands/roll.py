8a0743ef6fc414d3e506c5cb4ccd275324cc6279
Hansennico <nicolashansen@gmail.com>
2025-02-21 03:05:26 +0700

    update #2

--- cogs/Commands/roll.py
+++ cogs/Commands/roll.py
@@ -10,21 +10,21 @@ class Roll(commands.Cog):
     @cooldown(1, 5, BucketType.user)
     @commands.command()
     async def roll(self, ctx, x: str = None):
-        user = ctx.author.display_name # dapetin nama user yang make command nya
+        # Get Username
+        user = ctx.author.display_name
         print(colored(user, 'red'), "use roll command")
         if x is None:
-            # kalo angka nya gk di masukin random ampe 100
+            # Default Value
             await ctx.send(f"**{user}** roll, and get {random.randint(1, 100)}")
         else:
             try:
                 x = int(x)
                 if x < 1:
-                    # cek kalo angka nya di bawah 1
                     await ctx.send("please provide a positive number greater than 0.")
                     return
                 await ctx.send(f"**{user}** roll, and get {random.randint(1, x)}")
             except:
-                # cek kalo angka nya gk wajar
+                # Invalid Number
                 await ctx.send("Please provide a valid Number.")
 
 async def setup(bot):
